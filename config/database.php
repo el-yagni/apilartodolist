0ff8ab158d2c1c930fbed83ab08fb98da476345e
el-yagni <niqueepremiere@gmail.com>
2024-09-06 14:58:52 +0700

    tambah api baru

--- config/database.php
+++ config/database.php
@@ -31,22 +31,16 @@ return [
 
     'connections' => [
 
-        'sqlite' => [
-            'driver' => 'sqlite',
-            'url' => env('DB_URL'),
-            'database' => env('DB_DATABASE', database_path('database.sqlite')),
-            'prefix' => '',
-            'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
-        ],
+  
 
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'laravel'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => env('DB_HOST', '6-4.h.filess.io'),
+            'port' => env('DB_PORT', '3307'),
+            'database' => env('DB_DATABASE', 'backend_respectfar'),
+            'username' => env('DB_USERNAME', 'backend_respectfar'),
+            'password' => env('DB_PASSWORD', 'ee0b1191a9109e13ebdc0b774592b02aca3f5d7d'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => env('DB_CHARSET', 'utf8mb4'),
             'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
