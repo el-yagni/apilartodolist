797f26a14ad497aa5f1afdf8ea54280b24dec914
el-yagni <niqueepremiere@gmail.com>
2024-09-06 18:34:40 +0700

    bro pliss dah

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -18,7 +18,7 @@ class AuthController extends Controller
     public function register(Request $request)
     {
         $validator = Validator::make($request->all(), [
-            "name" => "required|required",
+            "name" => "required|string",
             "email" => "required|email|unique:users,email",
             "password" => "required|password",
             "confirm_password" => "required|same:password"
